5a1a5b5682b7f8940b373cc31a9dcfd809febf1f
Yang <yyqyang222@163.com>
2018-10-26 18:14:22 +0800

    add：ajax，重构登录注册还有bug

--- ser/public/javascripts/userController.js
+++ ser/public/javascripts/userController.js
@@ -1,3 +1,4 @@
+"use strict";
 
 function checkEmail(str) {
     var rule = /^[0-9a-zA-Z]+@[0-9a-zA-Z]+\.[a-zA-Z]{2,3}$/;
@@ -8,32 +9,99 @@ function checkPassword(pass, confirmPass) {
     return pass === confirmPass ;
 }
 
-function login() {
-    console.log($("input[name='username']"));
-    if (!checkEmail($('input[name="username"]').val())) {
+/**
+ * 前端调用的登录方法
+ */
+$("#login").click(function () {
+    var username = $("input[name='username']");
+    var password = $("input[name='password']");
+
+    if (!checkEmail(username.val())) {
         alert('邮箱格式不正确');
         return;
     }
-    if (($("input[name='password']").val()) === '') {
+    if ((password.val()) === '') {
         alert('请填写密码');
         return;
     }
     //check password
-    window.location.href = "/home";
+    $.ajax({
+        url: '/login',
+        type: 'post',
+        data: $(".login_form").serialize(),
+        success: function (data) {
+           if(data.status === 99999) {
+               window.location.href('/home');
+           } else {
+               alert('用户名或密码错误');
+           }
+        }
+    });
+});
+
+/**
+ * 查数据库验证登录,连接数据库
+ * @param username
+ * @param password
+ */
+function loginCheck(username, password) {
+    var loginSql = 'select password from user where username=' + username + ';';
+    connection.query(loginSql, function (err, res) {
+        if(err) {
+            console.log('[INSERT ERROR] - ',err.message);
+        } else {
+            if(res === password) {
+                console.log('登录成功！')
+                return true;
+            } else {
+                return false;
+            }
+        }
+    })
 }
 
+/**
+ * 前端调用的注册方法
+ */
 function register() {
-    if (!checkEmail($('input[name="username"]').val())) {
+    var username = $("input[name='username']");
+    var password = $("input[name='password']");
+    var confirmPass = $("input[name='confirmPassword']");
+
+    if (!checkEmail(username.val())) {
         alert('邮箱格式不正确');
         return;
     }
-    if(!checkPassword($('input[name="password"]').val(), $('input[name="confirmPassword"]').val())){
+    if(!checkPassword(password.val(), confirmPass.val())){
         alert('两次输入的密码不相同')
         return;
     }
-    if (($("input[name='password']").val()) === '' || ($("input[name='confirmPassword']").val()) === '') {
+    if ((password.val()) === '' || (confirmPass.val()) === '') {
         alert('请填写密码');
     }
-    //check password
-    window.location.href = "/";
+    //add user
+    var isSuccess = insertUser(username, password);
+    if (isSuccess) {
+        window.location.href = "/";
+    } else {
+        alert('注册失败，请重试！');
+    }
+}
+
+/**
+ * 加入用户
+ * @param connection
+ * @param username
+ * @param password
+ */
+function insertUser(username, password) {
+    var regisSql = 'insert into user(username,password) values(' + username + ',' + password + ');';
+    connection.query(regisSql, function (err, res) {
+        if(err) {
+            console.log('[INSERT ERROR] - ',err.message);
+        } else {
+            console.log('注册成功')
+            return true;
+        }
+    })
 }
--- ser/routes/index.js
+++ ser/routes/index.js
@@ -1,23 +1,56 @@
 "use strict";
 var express = require('express');
 var router = express.Router();
+// 连接mysql数据库
+var mysql = require('mysql');
+// var connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '15078852107yyq',
+//     port: '3306',
+//     database: 'memento'
+// });
+// connection.connect(function (err) {
+//     if(err) {
+//         console.log(err.message);
+//     } else {
+//         console.log("成功连接数据库！");
+//     }
+// });
+var sql = require('./sqlController');
 
 router.route('/')
     .get(function (req, res) {
         res.render('LoginPage', {title: '用户登录'});
-    })
+    });
+
+router.route('/login')
     .post(function (req, res) {
-        var user = {
-          username: 'admin',
-          password: '123456'
-        }
-        if(req.body.username === user.username && req.body.password === user.password) {
-            req.session.user = user;
-            res.redirect('/');
-        } else {
-            req.session.error('用户名或密码不正确');
-            res.redirect('/');
-        }
+        console.log('运行到这啦！！！')
+        console.log(req.body);
+        // var isSuccess = loginCheck(req.body.username, req.body.password);
+        // if(isSuccess) {
+        //     req.session.user = {
+        //         username: req.body.username,
+        //         password: req.body.password
+        //     };
+        //     res.redirect('/');
+        // } else {
+        //     req.session.error('用户名或密码不正确');
+        //     res.redirect('/');
+        // }
+        var username = req.body.username;
+        var password = req.body.password;
+        sql.select('SELECT * FROM user WHERE username = "'+ name + '" AND password = "' + password + '";').then(function(data) {
+            //session存user name和userid
+            console.log(data);
+            req.session.user = username;
+            data.status < 400 ? data.url = '/' : null;
+            res.json(data);
+            console.log(res);
+            res.end();
+            req.redirect("/");
+        }).catch(function(err){})
     });
 
 router.get('/logout', function (req, res) {
@@ -50,6 +83,27 @@ router.get('/register', function (req, res) {
         req.session.user = user;
         res.redirect('/');
     });
+
+/**
+ * 查数据库验证登录,连接数据库
+ * @param username
+ * @param password
+ */
+function loginCheck(username, password) {
+    var loginSql = 'select password from user where username=' + username + ';';
+    connection.query(loginSql, function (err, res) {
+        if(err) {
+            console.log('[INSERT ERROR] - ',err.message);
+        } else {
+            if(res === password) {
+                console.log('登录成功！')
+                return true;
+            } else {
+                return false;
+            }
+        }
+    })
+}
 
 /**
  * 检测访问权限
